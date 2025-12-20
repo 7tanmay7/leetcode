214efcf812c1b0192b1687f6858516601668a279
7tanmay7 <2405169@kiit.ac.in>
2025-12-20 15:26:42 +0530

    Time: 20 ms (70.42%), Space: 139.2 MB (39.57%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> result;
-        for(int i = 0; i < nums.size(); ++i) {
-            // Remove out-of-window indices
-            if(!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-            // Remove smaller values from back
-            while(!dq.empty() && nums[dq.back()] < nums[i])
-                dq.pop_back();
-            dq.push_back(i);
-            // Add to result from k-1 onwards
-            if(i >= k - 1)
+   deque<int>dq;
+   vector<int> result;
+   for (int i=0; i<nums.size();i++){
+    if (!dq.empty() && dq.front() <= i-k) {
+        dq.pop_front();
+    }
+    while (!dq.empty()&& nums [dq.back()]<nums[i]){
+        dq.pop_back();
+    }
+    dq.push_back(i);
+
+            // Once the first window is completed, add front element to result
+            if (i >= k - 1) {
                 result.push_back(nums[dq.front()]);
+            }
         }
+
+        // Return the final result
         return result;
     }
 };
