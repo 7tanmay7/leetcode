c54590b3de0bf7347db78c733afebb7609e1b483
7tanmay7 <2405169@kiit.ac.in>
2026-01-12 10:33:16 +0530

    Time: 0 ms (100%), Space: 23.5 MB (99.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,23 +1,16 @@
-//  Approach (Brute Force/Basic Recursion)
-// For every node, calculate the longest path that goes through it:
-// Find the depth of its left subtree.
-// Find the depth of its right subtree.
-// The sum of these depths is the diameter passing through that node.
-// Track the maximum diameter found during traversal
+
 class Solution {
 public:
- int maxDiameter = 0;
- //helper function
- int depth (TreeNode *node) {
-    if (!node) return 0;
-    int left = depth(node-> left);
-    int right = depth (node->right);
- // update the maxium diamenter
-  maxDiameter = max(maxDiameter , left+ right);
-  return 1+ max(left,right);
- }
-    int diameterOfBinaryTree(TreeNode* root) {
-     depth (root);
-     return maxDiameter;   
+    int maxDepth (TreeNode* root , int&maxi){
+        if(root== nullptr)return 0;
+        int left = maxDepth(root->left,maxi);
+        int right =maxDepth(root->right,maxi);
+        maxi =std::max(maxi,left+right);
+        return 1+ std::max(left, right);
+    }
+    int diameterOfBinaryTree(TreeNode* root){
+        int maxi=0;
+        maxDepth(root,maxi);
+        return maxi;
     }
 };
