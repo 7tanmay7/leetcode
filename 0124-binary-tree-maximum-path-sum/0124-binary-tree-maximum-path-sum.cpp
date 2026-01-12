52adaf916e9539bbab65c068e36be403f3fd7be3
7tanmay7 <2405169@kiit.ac.in>
2026-01-13 00:46:25 +0530

    Time: 0 ms (100%), Space: 28 MB (21.39%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,21 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        int maxSum = INT_MIN;
+        int maxSum INT_MIN;
         pathSum(root, maxSum);
         return maxSum;
     }
+    int pathSum (TreeNode*root , int &maxSum){
+        if(!root)return 0;
+          int left= max(0, pathSum(root->left,maxSum));
 
-    int pathSum(TreeNode* root, int &maxSum) {
-        if (!root) return 0;
+          int right =max(0, pathSum(root->right,maxSum));
+          maxSum = max(maxSum , root-> val+left+right);
+          return root->val+ max(left,right);
 
-        int left = max(0, pathSum(root->left, maxSum));   // Ignore negatives
-        int right = max(0, pathSum(root->right, maxSum)); // Ignore negatives
-
-        // Update maxSum: may pass through root (left + root + right)
-        maxSum = max(maxSum, root->val + left + right);
-
-        // Return max single path upward
-        return root->val + max(left, right);
     }
 };
