58b4c6de3dd0b906abcebdb0607d19db73307c66
7tanmay7 <2405169@kiit.ac.in>
2026-01-13 00:31:36 +0530

    Time: 0 ms (100%), Space: 23.9 MB (53.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int j = -1;
-
-        // Step 1: Find the index of the first 0
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
+        int j=0;
+        for(int i=0; i<nums.size();i++){
+            if (nums[i]!=0){
+                swap (nums[i], nums[j]);
+                j++;
             }
         }
-
-        // If there's no zero, array is already fine
-        if (j == -1) return;
-
-        // Step 2: From the next index, look for non-zero and swap with j
-        for (int i = j + 1; i < n; i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++; // Move j to next zero
-            }
-        }
+        
     }
 };
