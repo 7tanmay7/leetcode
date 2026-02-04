ca576e720ed8cbbdbad54d030e2b07f7c1862767
7tanmay7 <2405169@kiit.ac.in>
2026-02-04 21:27:37 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    int fmemo(int i, vector<int>& nums, vector<int>& dp) {
+        if (i < 0)
+            return 0;
+        if (i == 0)
+            return dp[i] = nums[i];
+        if (dp[i] != -1)
+            return dp[i];
+        int take = nums[i] + fmemo(i - 2, nums, dp);
+        int notake = fmemo(i - 1, nums, dp);
+        return dp[i] = max(take, notake);
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        long long int prev = nums[0];
-        long long int prev2 = 0;
-
-        for (int i = 1; i < n; i++) {
-            long long int pick = nums[i];
-            if (i > 1) pick += prev2;
-            long long int nonPick = prev;
-            long long int cur_i = max(pick, nonPick);
-            prev2 = prev;
-            prev = cur_i;
-        }
-        return prev;
+        vector<int> dp(n, -1);
+        return fmemo(n - 1, nums, dp);
     }
 };
