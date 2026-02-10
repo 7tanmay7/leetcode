1e4b4e4bb16b25b55d1a3dfb7506c78c7068f387
7tanmay7 <2405169@kiit.ac.in>
2026-02-10 23:33:08 +0530

    Time: 3 ms (63.89%), Space: 17.1 MB (95.97%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);
-        for (int i = 0; i < flights.size(); i++) {
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int w = flights[i][2];
-            graph[u].push_back(make_pair(v, w));
-        }
-        
-        vector<int> cost(n, INT_MAX);
-        queue<pair<int, pair<int, int>>> q; // {current city, {current cost, stops}}
-        q.push(make_pair(src, make_pair(0, 0)));
+        vector<int> cost(n, 1e9);
         cost[src] = 0;
-        
-        while (!q.empty()) {
-            int city = q.front().first;
-            int currCost = q.front().second.first;
-            int stops = q.front().second.second;
-            q.pop();
-            
-            if (stops > k) continue;
-            
-            for (int i = 0; i < graph[city].size(); i++) {
-                int next = graph[city][i].first;
-                int price = graph[city][i].second;
-                if (currCost + price < cost[next]) {
-                    cost[next] = currCost + price;
-                    q.push(make_pair(next, make_pair(cost[next], stops + 1)));
+
+        for (int i = 0; i <= k; ++i) {
+            vector<int> temp = cost;
+            for (auto& flight : flights) {
+                int u = flight[0], v = flight[1], w = flight[2];
+                if (cost[u] != 1e9 && cost[u] + w < temp[v]) {
+                    temp[v] = cost[u] + w;
                 }
             }
+            cost = temp;
         }
-        return cost[dst] == INT_MAX ? -1 : cost[dst];
+        return cost[dst] == 1e9 ? -1 : cost[dst];
     }
 };
