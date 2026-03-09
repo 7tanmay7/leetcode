e8e4dd16aaf313cdc801644cdfccdb8607ee762c
7tanmay7 <2405169@kiit.ac.in>
2026-03-09 10:07:44 +0530

    Time: 0 ms (100%), Space: 95.2 MB (62.2%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Make sure nums1 is the smaller array
-        if (nums1.size() > nums2.size())
+        
+        if(nums1.size() > nums2.size())
             return findMedianSortedArrays(nums2, nums1);
 
-        int m = nums1.size(), n = nums2.size();
-        int left = 0, right = m;
-        while (left <= right) {
-            int i = left + (right - left) / 2;
-            int j = (m + n + 1) / 2 - i;
+        int m = nums1.size();
+        int n = nums2.size();
 
-            int maxLeftA = (i == 0) ? INT_MIN : nums1[i - 1];
-            int minRightA = (i == m) ? INT_MAX : nums1[i];
-            int maxLeftB = (j == 0) ? INT_MIN : nums2[j - 1];
-            int minRightB = (j == n) ? INT_MAX : nums2[j];
+        int low = 0;
+        int high = m;
 
-            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                if ((m + n) % 2 == 0)
-                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+        while(low <= high){
+            int cut1 = (low + high) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            int l1, l2, r1, r2;
+
+            if(cut1 == 0)
+                l1 = INT_MIN;
+            else
+                l1 = nums1[cut1 - 1];
+
+            if(cut2 == 0)
+                l2 = INT_MIN;
+            else
+                l2 = nums2[cut2 - 1];
+
+            if(cut1 == m)
+                r1 = INT_MAX;
+            else
+                r1 = nums1[cut1];
+
+            if(cut2 == n)
+                r2 = INT_MAX;
+            else
+                r2 = nums2[cut2];
+
+            if(l1 <= r2 && l2 <= r1){
+                if((m + n) % 2 == 0)
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
                 else
-                    return double(max(maxLeftA, maxLeftB));
-            } else if (maxLeftA > minRightB) {
-                right = i - 1;
-            } else {
-                left = i + 1;
+                    return max(l1, l2);
+            }
+            else if(l1 > r2){
+                high = cut1 - 1;
+            }
+            else{
+                low = cut1 + 1;
             }
         }
-        return 0.0; // Should never reach here
+
+        return 0;
     }
 };
