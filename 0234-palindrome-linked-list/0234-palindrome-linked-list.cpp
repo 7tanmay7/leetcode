de70973e89143cfb2b0822d159655299d0e5f4f6
7tanmay7 <2405169@kiit.ac.in>
2026-04-03 08:47:58 +0530

    Time: 4 ms (49.87%), Space: 117.9 MB (93.72%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,32 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if (!head || !head->next) return true;
-        
-        // 1. Find the middle (slow/fast)
+
+        // Step 1: Find the middle using slow and fast pointers
         ListNode *slow = head, *fast = head;
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        // 2. Reverse the second half
+        // Step 2: Reverse the second half of the list
         ListNode* prev = nullptr;
-        while (slow) {
-            ListNode* next = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = next;
+        ListNode* curr = slow;
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
         }
 
-        // 3. Compare both halves
-        ListNode* left = head;
-        ListNode* right = prev;
-        while (right) {
-            if (left->val != right->val) return false;
-            left = left->next;
-            right = right->next;
+        // Step 3: Compare the first half and the reversed second half
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = prev; // 'prev' is now the head of the reversed half
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) return false;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
         }
+
         return true;
     }
 };
+
+    
