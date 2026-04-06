4638a15fc43cf59c573b35c85f4aeab236467dea
7tanmay7 <2405169@kiit.ac.in>
2026-04-06 17:10:18 +0530

    Time: 0 ms (100%), Space: 14.7 MB (53.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,25 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Hash map to store the value and its corresponding index
+        std::unordered_map<int, int> numMap;
+        
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
-            if (seen.count(complement)) {
-                return {seen[complement], i};
+            
+            // If complement exists in map, we found the solution
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
             }
-            seen[nums[i]] = i;
+            
+            // Otherwise, store the current number and its index
+            numMap[nums[i]] = i;
         }
-        return {}; // Should never reach here as per problem statement
+        
+        // Return empty vector if no solution exists (per constraints, this won't happen)
+        return {};
     }
 };
