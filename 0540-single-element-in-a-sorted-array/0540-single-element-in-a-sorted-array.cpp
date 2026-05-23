ab43eacfca3fe0aa21916a388f41eef8e967b086
7tanmay7 <2405169@kiit.ac.in>
2026-05-23 05:40:00 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.42%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,21 +1,24 @@
+#include <vector>
+
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-
-        while(low < high){
-            int mid = low + (high - low) / 2;
-
-            if(mid % 2 == 1) mid--;
-
-            if(nums[mid] == nums[mid + 1]){
-                low = mid + 2;
+    int singleNonDuplicate(std::vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+        
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            
+            // m ^ 1 cleanly checks the correct duplicate partner index:
+            // - If m is even, m ^ 1 is m + 1
+            // - If m is odd, m ^ 1 is m - 1
+            if (nums[m] == nums[m ^ 1]) {
+                l = m + 1; 
             } else {
-                high = mid;
-            }
+                r = m;                 }
         }
-
-        return nums[low];
+        
+        return nums[l];
     }
 };
