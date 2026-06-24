80c793ecd5667cf0945dc56e5d7e7f080cb446da
7tanmay7 <2405169@kiit.ac.in>
2026-06-24 20:26:53 +0530

    Time: 32 ms (58.27%), Space: 72.5 MB (14.83%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
-    public:
-    int findKthLargest(vector<int> & nums, int k){
-        priority_queue <int, vector<int> , greater<int>> minHeap;
-        for( int num: nums){
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>,greater<int>>minHeap;
+
+        for(int num :nums){
             minHeap.push(num);
-            if (minHeap.size() > k){
+            if(minHeap.size()>k){
                 minHeap.pop();
             }
-
-            }
-            return minHeap.top();
         }
-    };
+        return minHeap.top();
+        
+    }
+};
