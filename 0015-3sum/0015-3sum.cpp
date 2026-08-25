67e63d7eb46d863c82553af059f6c8304f94480a
7tanmay7 <2405169@kiit.ac.in>
2026-08-25 22:33:11 +0530

    Time: 45 ms (70.58%), Space: 29.2 MB (46.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-
-        for(int i =0; i<n-2; i++){
-            if(i > 0 && nums[i]== nums[i-1])
-                continue;
-
-            if(nums[i]>0) {
-                break;
+        sort(nums.begin(), nums.end()); 
+        vector<vector<int>> ans;
+        
+        int n = nums.size(); 
+        
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue; 
             }
-
-            int left = i+1;
-            int right = n-1;
-
-            while (left < right){
-                int sum = nums[i]+nums[left]+ nums[right];
-
-                if( sum ==0){
-                    result.push_back({nums[i], nums[left] , nums [right]});        
-                    
-                    while ( left < right && nums[left]== nums[left+1])
-                        left++;
-
-                    while(left < right && nums[right]== nums[right-1])
-                        right--;
-
+            
+            int left = i + 1, right = n - 1; 
+            
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]}); // Semicolon added here
                     left++;
                     right--;
+                    
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                 } 
-                else if (sum <0){
+                else if(sum < 0) {
                     left++;
-                }
+                } 
                 else {
                     right--;
                 }
-            }
+            } // The while loop properly closes here
         }
-        return result;
+        
+        return ans; 
     }
 };
